478404c1bbe9a82d8705306641dd19d4c0875e6c
JeremyKnight <jeremyknightjck99@gmail.com>
2017-11-09 09:29:04 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,12 +3,14 @@ import os
 import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+allStates=""
 
 @app.route("/")
 def render_main():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    return render_template('index.html', get_state_options(counties))
+        state = 
+        return render_template('index.html', "state_name" = get_state_options(counties) "fun_fact"=state_with_most_counties(counties, state))
     #return render_template('index.html')
 
 def get_state_options(counties):
@@ -19,10 +21,11 @@ def get_state_options(counties):
             states.append(c["State"])
             #state_name and fun_fact
             options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+    allStates = states
     return options
 
 def state_with_most_counties(counties, state):
-    """Return a the state with the average number of percent under of 18"""
+    """Return the state with the average number of percent under of 18"""
     count=0
     i=0
     for c in counties:
